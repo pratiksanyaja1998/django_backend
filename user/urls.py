61790e372a99900c6a23f7e9e70d96bb09f36ed8
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2023-10-13 09:47:15 +0530

    List user API

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from user.views import RegisterView, ChangePasswordView, UpdateProfileView, LogoutView, LogoutAllView, LoginView
+from user.views import RegisterView, ChangePasswordView, UpdateProfileView, UserListView, LoginView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='user_register'),
     path('change/password/<int:pk>/', ChangePasswordView.as_view(), name='user_change_password'),
     path('update/profile/<int:pk>/', UpdateProfileView.as_view(), name='user_update_profile'),
-    # path('user/list/', UserListView.as_view(), name="user_list"),
+    path('user/list/', UserListView.as_view(), name="user_list"),
     # path('logout/', LogoutView.as_view(), name='user_logout'),
     # path('logout_all/', LogoutAllView.as_view(), name='user_logout_all'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from .serializers import RegisterSerializer, ChangePasswordSerializer, UpdateUserSerializer
+from .serializers import RegisterSerializer, ChangePasswordSerializer, UpdateUserSerializer, ListUserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import generics
 from user.models import User
@@ -41,26 +41,34 @@ class UpdateProfileView(generics.UpdateAPIView):
     serializer_class = UpdateUserSerializer
 
 
-class LogoutView(APIView):
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
     permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ListUserSerializer
 
 
-class LogoutAllView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        tokens = OutstandingToken.objects.filter(user_id=request.user.id)
-        for token in tokens:
-            t, _ = BlacklistedToken.objects.get_or_create(token=token)
-
-        return Response(status=status.HTTP_205_RESET_CONTENT)
+#
+#
+# class LogoutView(APIView):
+#     permission_classes = (IsAuthenticated,)
+#
+#     def post(self, request):
+#         try:
+#             refresh_token = request.data["refresh_token"]
+#             token = RefreshToken(refresh_token)
+#             token.blacklist()
+#
+#             return Response(status=status.HTTP_205_RESET_CONTENT)
+#         except Exception as e:
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class LogoutAllView(APIView):
+#     permission_classes = (IsAuthenticated,)
+#
+#     def post(self, request):
+#         tokens = OutstandingToken.objects.filter(user_id=request.user.id)
+#         for token in tokens:
+#             t, _ = BlacklistedToken.objects.get_or_create(token=token)
+#
+#         return Response(status=status.HTTP_205_RESET_CONTENT)
